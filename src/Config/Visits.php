20c45d92144613fc597df8a7d90ccfcb524311a3
MGatner <mgatner@icloud.com>
2022-07-05 14:48:29 +0000

    Refactor config options

--- src/Config/Visits.php
+++ src/Config/Visits.php
@@ -8,6 +8,28 @@ use Tatter\Visits\Interfaces\Transformer;
 class Visits extends BaseConfig
 {
     /**
+     * Database field for tracking a unique visitor
+     *
+     * @var 'ip_address'|'session_id'|'user_id'
+     */
+    public string $trackingMethod = 'ip_address';
+
+    /**
+     * Number of seconds before a visit counts as new
+     * instead of incrementing a previous view count.
+     * Set to zero to record each page view as unique (not recommended).
+     */
+    public int $resetAfter = HOUR;
+
+    /**
+     * Whether to ignore AJAX requests when recording.
+     * See framework User Guide for caveats.
+     *
+     * @see https://www.codeigniter.com/user_guide/general/ajax.html
+     */
+    public bool $ignoreAjax = true;
+
+    /**
      * Transformers to apply (in order) before
      * recording the visit data.
      *
@@ -16,53 +38,4 @@ class Visits extends BaseConfig
      * @var class-string<Transformer>[]
      */
     public array $transformers = [];
-
-    /**
-     * Whether to enable tracking in all controllers using
-     * the post_controller_constructor event.
-     */
-    public bool $trackAllPages = true;
-
-    /**
-     * Metric for tracking a unique visitor
-     *
-     * @var 'ip_address'|'session_id'|'user_id'
-     */
-    public $trackingMethod = 'ip_address';
-
-    /**
-     * Session variable to check for a logged-in user ID
-     *
-     * @deprecated Next version will rely on codeigniter4/authentication-implementation
-     *
-     * @var string
-     */
-    public $userSource = 'logged_in';
-
-    /**
-     * Number of minutes before a visit counts as new
-     * instead of incrementing a previous view count.
-     * Set to zero to record each page view as unique (not recommended).
-     *
-     * @var int
-     */
-    public $resetMinutes = 60;
-
-    /**
-     * Whether to ignore AJAX requests when recording.
-     * See framework User Guide for caveats.
-     *
-     * @see https://www.codeigniter.com/user_guide/general/ajax.html
-     *
-     * @var bool
-     */
-    public $ignoreAjax = true;
-
-    /**
-     * URIs to exclude from tracking.
-     * Accepts regex values.
-     *
-     * @var string[]
-     */
-    public array $excludeUris = [];
 }
